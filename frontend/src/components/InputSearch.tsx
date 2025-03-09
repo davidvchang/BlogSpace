224e245f524c65df6e98198fbff829f5866cd97b
davidvchang <dvalenzuelachang@gmail.com>
2025-03-09 02:26:54 -0700

    Add: placeholder search blog

--- frontend/src/components/InputSearch.tsx
+++ frontend/src/components/InputSearch.tsx
@@ -4,7 +4,7 @@ import { Search } from 'lucide-react';
 const InputSearch:React.FC = () => {
   return (
     <div className='flex w-fit h-fit relative'>
-        <input type="search" name="search" id="search" className='w-64 px-8 py-2 border border-slate-300 rounded-md font-light'/>
+        <input type="search" name="search" id="search" className='w-64 px-8 py-2 border border-slate-300 rounded-md font-light' placeholder='Search...'/>
         <Search className='w-4 h-4 absolute top-3 left-2 text-slate-500'/>
     </div>
   )
