3142cf37be72531d11912a912511fb7a90a06896
davidvchang <dvalenzuelachang@gmail.com>
2025-03-09 23:05:06 -0700

    Add: endpoinds getOneBlog and deleteBlog

--- backend/src/controllers/blogs.controller.js
+++ backend/src/controllers/blogs.controller.js
@@ -19,3 +19,29 @@ export const postBlog = async (req, res) => {
         res.status(500).json("An error has ocurred: ", ex)
     }
 }
+
+export const getOneBlog = async (req, res) => {
+    const {id_blog} = req.query
+    try {
+        const existBlog = await pool.query("SELECT COUNT(*) FROM blogs WHERE id_blog = $1", [id_blog])
+        if(existBlog.rows[0].count > 0) {
+            const blogs = await pool.query("SELECT * FROM blogs WHERE id_blog = $1", [id_blog])
+            res.status(200).json(blogs.rows)
+        }
+    } catch (ex) {
+        res.status(500).json("An error has ocurred: ", ex)
+    }
+}
+
+export const deleteBlog = async (req, res) => {
+    const {id_blog} = req.query
+    try {
+        const existBlog = await pool.query("SELECT COUNT(*) FROM blogs WHERE id_blog = $1", [id_blog])
+        if(existBlog.rows[0].count > 0) {
+            await pool.query("DELETE FROM blogs WHERE id_blog = $1", [id_blog])
+            res.status(204).json("Blog has been deleted correctly")
+        }
+    } catch (ex) {
+        res.status(500).json("An error has ocurred: ", ex)
+    }
+}
--- backend/src/routes/blogs.routes.js
+++ backend/src/routes/blogs.routes.js
@@ -1,12 +1,12 @@
 import {Router} from 'express'
-import {getBlogs, postBlog} from '../controllers/blogs.controller.js'
+import {getBlogs, postBlog, getOneBlog, deleteBlog} from '../controllers/blogs.controller.js'
 
 const router = Router()
 
 router.get("/", getBlogs)
 router.post("/", postBlog)
-router.get("/:id_blog")
-router.delete("/:id_blog")
+router.get("/:id_blog", getOneBlog)
+router.delete("/:id_blog", deleteBlog)
 router.put("/:id_blog")
 
 export default router
