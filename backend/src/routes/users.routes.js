645bd4ee5c7420e61aea02d0230d4932600061c9
davidvchang <dvalenzuelachang@gmail.com>
2025-03-19 15:26:25 -0700

    Add: route autenticatedUser

--- backend/src/routes/users.routes.js
+++ backend/src/routes/users.routes.js
@@ -1,5 +1,6 @@
 import {Router} from 'express'
 import {getUsers, postUser, loginUser} from '../controllers/users.controller.js'
+import verifyToken from '../middlewares/verifyToken.js'
 
 const router = Router()
 
@@ -7,5 +8,9 @@ router.get("/", getUsers)
 router.post("/", postUser)
 
 router.post("/login", loginUser)
+
+router.get("/autenticatedUser", verifyToken, (req, res) => {
+    res.json(req.user); 
+});
 
 export default router
