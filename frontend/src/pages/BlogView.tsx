66654d50ae91947023e02d931b7f26765d03ef4b
davidvchang <dvalenzuelachang@gmail.com>
2025-03-19 22:38:29 -0700

    Fix: show author in BlogView page

--- frontend/src/pages/BlogView.tsx
+++ frontend/src/pages/BlogView.tsx
@@ -24,63 +24,81 @@ interface SectionsBlog {
     paragraphs: string[];
 }
 
+interface PropsInfoUser {
+    id_user: number,
+    email: string,
+    name: string,
+    last_name: string
+}
+
 const BlogView:React.FC = () => {
 
-    const URL_BLOGS:string = import.meta.env.VITE_URL_BLOGS 
+    const URL_BLOGS:string = import.meta.env.VITE_URL_BLOGS
+    const URL_USERS = import.meta.env.VITE_URL_USERS
 
     const {id_blog} = useParams()
 
     const [dataBlogs, setDataBlogs] = useState<DataBlogs[]>([])
+    const [dataUsers, setDataUsers] = useState<PropsInfoUser[]>([])
 
     const getBlogs = async () => {
         const res = await axios.get(URL_BLOGS + id_blog)
         setDataBlogs(res.data)
     }
 
+    const getNameUserOfBlog = async () => {
+        const res = await axios.get(URL_USERS)
+        setDataUsers(res.data)
+    }
+
     useEffect(() => {
       getBlogs()
+      getNameUserOfBlog()
     }, [])
   return (
     <section className='flex flex-col w-full items-center py-10 px-5 border-b border-b-slate-200'>
         <div className='flex flex-col w-[70%]'>
-            {dataBlogs.map((blog) => (
-                <div className='flex flex-col gap-4' key={blog.id_blog}>
-                    <Category text={blog.category} blue_color={true}/>
+            {dataBlogs.map((blog) => {
+                const authorBlog = dataUsers.find(user => user.id_user === blog.user_id);
+                return (
+                    <div className='flex flex-col gap-4' key={blog.id_blog}>
+                        <Category text={blog.category} blue_color={true}/>
 
-                    <div className='flex flex-col gap-3'>
-                        <span className='text-4xl font-bold'>{blog.title}</span>
-                        <span className='text-slate-500 text-2xl'>{blog.description}</span>
-                    </div>
+                        <div className='flex flex-col gap-3'>
+                            <span className='text-4xl font-bold'>{blog.title}</span>
+                            <span className='text-slate-500 text-2xl'>{blog.description}</span>
+                        </div>
 
-                    <div className='flex justify-between items-center pt-5'>
-                        <ProfilePostCard blog_view={true}/>
+                        <div className='flex justify-between items-center pt-5'>
+                            <ProfilePostCard blog_view={true} author={authorBlog && `${authorBlog.name} ${authorBlog.last_name}`}/>
 
-                        <div className='flex gap-5'>
-                            <InformationBlogCard name={blog.date.split("T")[0]}/> 
-                            <InformationBlogCard name='comments'/> 
+                            <div className='flex gap-5'>
+                                <InformationBlogCard name={blog.date.split("T")[0]}/> 
+                                <InformationBlogCard name='comments'/> 
+                            </div>
+
+                        </div>
+                        <div className='flex flex-col gap-20 py-10 border-b border-b-slate-200'>
+                            {/* IMAGE */}
+                            <div className='w-full h-[28rem] rounded-lg overflow-hidden'>
+                                <img src={blog.image_url} alt="Cover Image" className='w-full h-full object-cover'/>
+                            </div>
+
+                            <div className='flex flex-col gap-20'>
+                                {blog.sections.map((section, index) => {
+                                    const paragraphs = Array.isArray(section.paragraphs) ? section.paragraphs : [section.paragraphs];
+                                    
+                                    return (
+                                        <SectionBlog key={index} title={section.title} paragraphs={paragraphs}/>
+
+                                    )
+                                })}
+                            </div>
                         </div>
 
                     </div>
-                    <div className='flex flex-col gap-20 py-10 border-b border-b-slate-200'>
-                        {/* IMAGE */}
-                        <div className='w-full h-[28rem] rounded-lg overflow-hidden'>
-                            <img src={blog.image_url} alt="Cover Image" className='w-full h-full object-cover'/>
-                        </div>
-
-                        <div className='flex flex-col gap-20'>
-                            {blog.sections.map((section, index) => {
-                                const paragraphs = Array.isArray(section.paragraphs) ? section.paragraphs : [section.paragraphs];
-                                
-                                return (
-                                    <SectionBlog key={index} title={section.title} paragraphs={paragraphs}/>
-
-                                )
-                            })}
-                        </div>
-                    </div>
-
-                </div>
-            ))}
+                )
+            })}
 
             <div className='flex justify-end items-center py-3 border-b border-b-slate-200'>
                 <div className='flex gap-1 items-center w-fit h-fit px-3 py-2 rounded-md hover:bg-slate-100 hover:transition duration-300'>
